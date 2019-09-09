46b8a966290d5be47c58e2327552f6b0a117e38b
hyeontak1 <thk4491998@gmail.com>
2019-09-09 19:27:27 +0900

    db모델

---
+++ src/main/java/tour/list/web/model/search_Festival.java
@@ -0,0 +1,27 @@
+package tour.list.web.model;
+
+import lombok.Data;
+
+@Data
+public class search_Festival {
+
+	private String resultCode;
+	private String resultMsg;
+	private String numOfRows;
+	private String pageNo;
+	private String totalCount;
+	private String addr1;
+	private String addr2;
+	private String cat1;
+	private String cat2;
+	private String cat3;
+	private String contentid;
+	private String contentypeid;
+	private String createdtime;
+	private String eventstartdate;
+	private String eventenddate;
+	private String firstimage;
+	private String modifiedtime;
+	private String title;
+
+}
---
+++ src/main/java/tour/list/web/model/search_stay.java
@@ -0,0 +1,25 @@
+package tour.list.web.model;
+
+import lombok.Data;
+
+@Data
+public class search_stay {
+
+	private String resultCode;
+	private String resultMsg;
+	private String numOfRows;
+	private String pageNo;
+	private String totalCount;
+	private String addr1;
+	private String addr2;
+	private String cat1;
+	private String cat2;
+	private String cat3;
+	private String contentid;
+	private String contentypeid;
+	private String createdtime;
+	private String firstimage;
+	private String modifiedtime;
+	private String title;
+	private String area_infomationcol;
+}
