4790bfa30237ad96326a16ba3f5a837b69a1e01c
hyeontak1 <thk4491998@gmail.com>
2019-09-09 19:03:31 +0900

    틀3

---
+++ src/main/java/tour/list/web/model/category_code.java
@@ -0,0 +1,16 @@
+package tour.list.web.model;
+
+import lombok.Data;
+
+@Data
+public class category_code {
+
+	private String resultCode;
+	private String resultMsg;
+	private String numOfRows;
+	private String pageNo;
+	private String totalCount;
+	private String code;
+	private String name;
+	private String rnum;
+}
