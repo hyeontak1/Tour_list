5f4965e8458b5a44c183d511d654038f3e32826b
hyeontak1 <thk4491998@gmail.com>
2019-09-16 17:30:13 +0900

    api 모델 완성2

---
+++ src/main/java/tour/list/web/model/searchKeywordApi.java
@@ -0,0 +1,56 @@
+package tour.list.web.model;
+
+import java.util.List;
+
+import com.google.gson.annotations.SerializedName;
+
+import lombok.Data;
+
+@Data
+public class searchKeywordApi {
+	   @SerializedName("response")
+	   private Response response;
+	   
+	  
+	   @Data
+	   public class Response {
+	      @SerializedName("header")      private List<Header> header;
+	      @SerializedName("body")        private List<Body> body;
+	      
+	   
+	  
+	      @Data
+	      public class Header {
+	         @SerializedName("resultCode")            private int resultCode;
+	         @SerializedName("resultMsg")      private String resultMsg;  
+	         }
+	      
+	      @Data
+	      public class Body{
+	          @SerializedName("items")      private List<Items> items;
+	         @SerializedName("numOfRows")            private int numOfRows;
+	          @SerializedName("pageNo")      private int pageNo;
+	          @SerializedName("totalCount")      private int totalCount;
+	          
+	          public class Items{
+	             @SerializedName("item")      private List<Item> item;
+	             
+	             public class Item{
+	                @SerializedName("addr1")            private String addr1;
+	                  @SerializedName("contentid")      private int contentid;
+	                  @SerializedName("contenttypeid")      private int contenttypeid;
+	                  @SerializedName("createdtime")      private String createdtime;
+	                  @SerializedName("firstimage")      private String firstimage;
+	                  @SerializedName("firstimage2")      private String firstimage2;
+	                  @SerializedName("modifiedtime")      private String modifiedtime;
+	                  @SerializedName("tel")      private String tel;
+	                  @SerializedName("title")      private String title;
+	                
+	             }
+	             
+	             
+	          }
+	         
+	      }
+	      }
+}
