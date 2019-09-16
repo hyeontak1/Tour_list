664ae14a814c4778ec450c721bf28f0b29ff218d
hyeontak1 <thk4491998@gmail.com>
2019-09-16 19:17:13 +0900

    api

---
+++ src/main/java/tour/list/web/service/apiService.java
@@ -0,0 +1,34 @@
+package tour.list.web.service;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import tour.list.web.model.detailCommonApi;
+import tour.list.web.model.detailImageApi;
+import tour.list.web.model.detailIntroApi;
+import tour.list.web.model.searchFestivalApi;
+
+public interface apiService {
+	
+	public static final String BASE_URL = "http://api.visitkorea.or.kr";
+
+	// 키워드 검색
+	@GET("/openapi/service/rest/KorService/searchKeyword?ServiceKey=l2VTTN0fU0JbAUod9iD%2BKG04GF%2BWIr4GohyuKRP22H8ujU1rlliUqGxlveADrCtqIGocNfUu%2FK3EzQMXgSjEYw%3D%3D&numOfRows=6&MobileOS=AND&MobileApp=TourList&_type=json")
+	Call<detailCommonApi> getDetailCommonApi(@Query("keyword") String keyword);
+	
+	// 행사 검색
+	@GET("/openapi/service/rest/KorService/searchFestival?ServiceKey=l2VTTN0fU0JbAUod9iD%2BKG04GF%2BWIr4GohyuKRP22H8ujU1rlliUqGxlveADrCtqIGocNfUu%2FK3EzQMXgSjEYw%3D%3D&numOfRows=6&MobileOS=AND&MobileApp=TourList&_type=json")
+	Call<searchFestivalApi> getSearchFestivalApi(@Query("eventStartDate") String eventStartDate);
+	
+	// 공통정보 조회
+	@GET("/openapi/service/rest/KorService/detailCommon?ServiceKey=l2VTTN0fU0JbAUod9iD%2BKG04GF%2BWIr4GohyuKRP22H8ujU1rlliUqGxlveADrCtqIGocNfUu%2FK3EzQMXgSjEYw%3D%3D&numOfRows=6&MobileOS=AND&MobileApp=TourList&_type=json")
+	Call<detailCommonApi> getDetailCommonApi(@Query("contentId") int contentId);
+	
+	// 소개정보
+	@GET("/openapi/service/rest/KorService/detailIntro?ServiceKey=l2VTTN0fU0JbAUod9iD%2BKG04GF%2BWIr4GohyuKRP22H8ujU1rlliUqGxlveADrCtqIGocNfUu%2FK3EzQMXgSjEYw%3D%3D&numOfRows=6&MobileOS=AND&MobileApp=TourList&_type=json")
+	Call<detailIntroApi> getDetailIntroApi(@Query("contentId") int contentId, @Query("contetnTypeId") int contetnTypeId);
+	
+	// 이미지
+	@GET("/openapi/service/rest/KorService/detailImage?ServiceKey=l2VTTN0fU0JbAUod9iD%2BKG04GF%2BWIr4GohyuKRP22H8ujU1rlliUqGxlveADrCtqIGocNfUu%2FK3EzQMXgSjEYw%3D%3D&numOfRows=6&MobileOS=AND&MobileApp=TourList&_type=json")
+	Call<detailImageApi> getDetailImageApi(@Query("contentId") int contentId);	
+}
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,14 +1,25 @@
+<%--jsp 페이지 헤더 선언 추가해야함 --%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+ <%@ page trimDirectiveWhitespaces="true" %>
+ <%--JSTL이 제공하는 문법적 기능을"c"라는 태그를 통해 공급받겠다는 선언 --%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%--이페이지에서 session을 사용할지 여부.기본값은 true --%>
 <%@ page session="false" %>
 <html>
 <head>
-	<title>Home</title>
-</head>
-<body>
-<h1>
-	Hello world!  
-</h1>
+   <title>Home</title>
+<meta charset="utf-8" />
+<%--"${pageContext.request.contextPath}" --%>
 
+<link rel="stylesheet" type=" text/css" href="${pageContext.request.contextPath}/assets/css/style.css"/>
+</head>
+
+<body>
+  <img src="${pageContext.request.contextPath}/assets/img/spring.png" width="320" />
+<h1>   Hello world!  </h1>
+<%--컨트롤러에서 model 객체를 통해 전달할 변수는 ${이름}형식으로 접근한다. --%>
 <P>  The time on the server is ${serverTime}. </P>
+<script type="text/css" src=" ${pageContext.request.contextPath}/assets/js/script.js"></script>
 </body>
 </html>
