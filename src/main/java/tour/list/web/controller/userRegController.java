7609741fb616a5204eba7031cebe0301c06dd137
hyeontak1 <thk4491998@gmail.com>
2019-09-18 14:52:02 +0900

    로그인

--- src/main/java/tour/list/web/controller/userRegController.java
+++ src/main/java/tour/list/web/controller/userRegController.java
@@ -24,8 +24,13 @@ public class userRegController {
 	@Value("#{servletContext.contextPath}")
 	String contextPath;
 	
-	@RequestMapping(value="/tour_list/userReg.do", method= RequestMethod.POST)
-	public ModelAndView userReg(Model model) {
+	@RequestMapping(value="/tour_list/userReg.do", method= RequestMethod.GET)
+	public String userReg(Model model) {
+		return "tour_list/userReg";
+	}
+	
+	@RequestMapping(value="/tour_list/userReg_ok.do", method= RequestMethod.POST)
+	public ModelAndView userReg_ok(Model model) {
 		String id = webHelper.getString("id");
 		String password=webHelper.getString("password");
 		String name=webHelper.getString("name");
@@ -38,6 +43,27 @@ public class userRegController {
 		
 		if(id==null) {
 			return webHelper.redirect(null, "아이디를 입력하세요.");
+		}
+		if(password==null) {
+			return webHelper.redirect(null, "비민번호를 입력하세요.");
+		}
+		if(name==null) {
+			return webHelper.redirect(null, "이름을 입력하세요.");
+		}
+		if(gender==null) {
+			return webHelper.redirect(null, "성별을 입력하세요.");
+		}
+		if(birth==null) {
+			return webHelper.redirect(null, "생일을 입력하세요.");
+		}
+		if(mail==null) {
+			return webHelper.redirect(null, "이메일을 입력하세요.");
+		}
+		if(phone==null) {
+			return webHelper.redirect(null, "휴대전화번호를 입력하세요.");
+		}
+		if(address==null) {
+			return webHelper.redirect(null, "주소를 입력하세요.");
 		}
 		
 		member input = new member();
